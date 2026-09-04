74a2df54e597189ae7a766dcaaa979a9f87ca05f
George Wild <git@georgewild.dev>
2026-09-04 21:35:30 +0100

    Added the wrangler

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// src/index.ts
+var index_default = {
+  async fetch(request, env, ctx) {
+    return new Response("Hello World!");
+  }
+};
+export {
+  index_default as default
+};
+//# sourceMappingURL=index.js.map
